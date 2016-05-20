c7aaffa43b28371c279c9f54e486c62adb7601e7
jdipallo <jdipallo@gmail.com>
2016-05-20 18:09:49 +0000

    Completed exercise...

--- js1.js
+++ js1.js
@@ -2,7 +2,14 @@ var someWord = prompt("Please enter something, anything!");
 
 if (someWord.length !=0) {
     var length = someWord.length;
-    alert("You said '" + someWord + "'. The length is: " + length + " Nice job!");
+    var thirdChar = someWord.charAt(2);
+    var lowerCase = someWord.toLowerCase();
+    var upperCase = someWord.toUpperCase();
+    var useTheWord = "I love " + someWord + "'s guitar playing";
+    var TwoThroughFour = someWord.substr(1, 3);
+
+    alert("You said " + someWord + "\nThe length is: " + length +"\nThe 3rd character is: " 
+            + thirdChar + "\nLowerCase: " + lowerCase + "\nUpperCase: " + upperCase + "\n" + useTheWord + "\nLetters 2-4 = " + TwoThroughFour);
 } else {
     alert("Why did not listen to me and enter something!");
 }
